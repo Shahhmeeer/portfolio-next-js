653fc0b207982b40bd24e3d3c9a4e1cab09f05ff
Shahmeer Asim <shahmeerasim1999@gmail.com>
2023-11-04 19:07:10 +0500

    fixed header height problem

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   const navlinks = ["Projects", "Whimsy", "Connect"];
 
   return (
-    <header className="z-10 px-4 md:px-0 dark:border-b-2 border-b-2 backdrop-blur-sm sticky top-0 flex items-center md:justify-evenly justify-between" style={{ minHeight: "8vh" }}>
+    <header className="z-10 px-4 md:px-0 dark:border-b-2 border-b-2 backdrop-blur-sm sticky top-0 flex items-center md:justify-evenly justify-between" style={{ minHeight: "8dvh" }}>
       <div>
         <h1>Shahmeer Asim</h1>
       </div>
